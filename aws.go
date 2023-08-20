ac497e1983c023f76db389556b767041fdce217a
tehseensajjadsb <tehseensajjadsb@gmail.com>
2023-08-20 23:28:07 +0500

    Add sts get-caller-identity handler

---
+++ aws.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+)
+
+func GetAwsAuthConfig(regionString string) aws.Config {
+	cfg, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(regionString))
+	if err != nil {
+		log.Fatalf("Unable to authenticate with AWS. %v", err)
+	}
+	return cfg
+}
+
+func GetCallerIdentity(cfg aws.Config) string {
+	stsClient := sts.NewFromConfig(cfg)
+	callerIdentityOutput, err := stsClient.GetCallerIdentity(context.TODO(), &sts.GetCallerIdentityInput{})
+	if err != nil {
+		log.Fatalf("Unable to retrieve current session details, %v", err)
+	}
+
+	return fmt.Sprintf("ACCOUNT_ID: %10v\nARN: %10v\nUSER_ID: %10v\n", *callerIdentityOutput.Account, *callerIdentityOutput.Arn, *callerIdentityOutput.UserId)
+}
--- main.go
+++ main.go
@@ -3,5 +3,6 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Tool Init")
+	awsAuthConfig := GetAwsAuthConfig("us-east-1")
+	fmt.Printf("%v", GetCallerIdentity(awsAuthConfig))
 }
