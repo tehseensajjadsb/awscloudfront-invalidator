c73446349bd4e84e6a222ca2b64aa47f5b537a73
tehseensajjadsb <tehseensajjadsb@gmail.com>
2023-08-21 00:52:14 +0500

    Return invalidation id from invalidate method

--- src/distribution.go
+++ src/distribution.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"log"
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront/types"
@@ -22,7 +21,7 @@ var (
 	quanitity       int32  = 1
 )
 
-func (dist Distribution) Invalidate(PathString string, client cloudfront.Client) (cloudfront.CreateInvalidationOutput, error) {
+func (dist Distribution) Invalidate(PathString string, client *cloudfront.Client) (string, error) {
 	invalidationBatch := &types.InvalidationBatch{
 		CallerReference: &callerReference,
 		Paths: &types.Paths{
@@ -37,9 +36,10 @@ func (dist Distribution) Invalidate(PathString string, client cloudfront.Client)
 
 	invalidationResult, err := client.CreateInvalidation(context.TODO(), &invalidationInput)
 	if err != nil {
-		log.Fatalf("Failed creating invalidation")
-		log.Fatalf("%v", err)
+		return "", err
 	}
 
-	return *invalidationResult, err
+	invalidation := invalidationResult.Invalidation
+	invalidationId := *invalidation.Id
+	return invalidationId, nil
 }
