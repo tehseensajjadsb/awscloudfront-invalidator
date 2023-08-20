6c0e5bcb2d6d15fe03f534ec8b8b48c53e59a511
tehseensajjadsb <tehseensajjadsb@gmail.com>
2023-08-21 00:53:35 +0500

    Add distribution invalidation to main program flow
    Add checks for values of distribution id and invalidation path args

--- src/main.go
+++ src/main.go
@@ -2,6 +2,10 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 )
 
 func main() {
@@ -13,5 +17,27 @@ func main() {
 		callerIdentityOutput := GetCallerIdentity(awsAuthConfig)
 		fmt.Print(callerIdentityOutput)
 		return
+	}
+
+	if CloudfrontDistributionId != "" {
+		cfnClient := cloudfront.NewFromConfig(awsAuthConfig)
+
+		distribution := Distribution{
+			Id: CloudfrontDistributionId,
+		}
+
+		if InvalidationPath == "" {
+			log.Fatal("You must provide a path to invalidate")
+			os.Exit(1)
+		}
+
+		invalidationId, err := distribution.Invalidate(InvalidationPath, cfnClient)
+		if err != nil {
+			log.Fatalf("Failed to invalidation distribution %v\n", CloudfrontDistributionId)
+			log.Fatal(err)
+			os.Exit(1)
+		}
+
+		fmt.Printf("InvalidationId: %10s\n", invalidationId)
 	}
 }
