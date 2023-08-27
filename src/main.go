5dcf3f5303f4e7bd078c15d1602994ca77b9403e
tehseensajjadsb <tehseensajjadsb@gmail.com>
2023-08-27 20:11:01 +0500

    Refactor cli args into main.go

--- src/main.go
+++ src/main.go
@@ -1,14 +1,30 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
+	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/service/cloudfront"
 )
 
+var (
+	Region                   string
+	CloudfrontDistributionId string
+	Paths                    string
+	GetCurrentProfileDetails bool
+)
+
+func Init() {
+	flag.StringVar(&Region, "region", "us-east-1", "AWS Region")
+	flag.BoolVar(&GetCurrentProfileDetails, "whoami", false, "Get current profile details and exit, similar to output of 'aws sts get-caller-identity'")
+	flag.StringVar(&CloudfrontDistributionId, "distribution-id", "", "Cloudfront distribution id")
+	flag.StringVar(&Paths, "paths", "", "Path(s) to invalidate")
+	flag.Parse()
+}
+
 func main() {
-	// Parse commandline flags
 	Init()
 	awsAuthConfig := GetAwsAuthConfig(Region)
 
@@ -25,11 +41,13 @@ func main() {
 			Id: CloudfrontDistributionId,
 		}
 
-		if InvalidationPath == "" {
+		if Paths == "" {
 			log.Fatal("You must provide a path to invalidate")
 		}
 
-		invalidationId, err := distribution.Invalidate(InvalidationPath, cfnClient)
+		invalidationPaths := strings.Split(Paths, ",")
+
+		invalidationId, err := distribution.Invalidate(invalidationPaths, cfnClient)
 		if err != nil {
 			log.Fatalf("Failed to invalidation distribution %v\n%v", CloudfrontDistributionId, err)
 		}
